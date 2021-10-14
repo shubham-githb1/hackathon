5cd5f24d084a4beb3999b6b66d4ef09e17512774
shreyansh kashayap <shreyanshkashyap08@gmail.com>
2021-10-14 11:23:58 +0530

    graph changes

---
+++ src/components/chart/Chartjs.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Pie } from 'react-chartjs-2'
+
+const Chartjs = () => {
+
+    const colours = [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+        'rgb(255, 99, 13)',
+        'rgb(225, 05, 86)',
+        'rgb(54, 162, 35)',
+  ]
+
+  const pieName  = ['buyer1','buyer2','buyer3','buyer4','buyer5']
+  const pieData = [23,45,67,34,12]
+
+    const data = {
+        labels: pieName,
+        datasets: [{
+          label: 'Manager Info',
+          data: pieData,
+          backgroundColor: colours,
+          hoverOffset: 4
+        }]
+    }
+    return (
+        
+        <div className="chart" style={{height : '250px', width : '960px', margin: 'auto', padding: '10px 60px 60px 60px'}} >
+            <h2 className="chartTitle">Buyer's Data</h2>
+            <Pie 
+              height={400}
+              width={600}
+              options = {{
+                maintainAspectRatio: false,
+              }}
+              data={data}
+            />
+        </div>
+    )
+}
+
+export default Chartjs
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,27 @@
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+<<<<<<< HEAD
+=======
+import Chartjs from "../../components/chart/Chartjs";
+>>>>>>> e83a228 (first commit)
 import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> e83a228 (first commit)
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
       <Chart data={userData} title="Agriculture performance." grid dataKey="Active User"/>
+<<<<<<< HEAD
+=======
+      <Chartjs/>
+>>>>>>> e83a228 (first commit)
       <div className="homeWidgets">
         <WidgetSm/>
         <WidgetLg/>
